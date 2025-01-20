87db1e00ff95882028153eee10e574a0769f2358
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2025-01-20 19:30:32 +0700

    add tesla for crawling data.

--- mix.exs
+++ mix.exs
@@ -58,7 +58,8 @@ defmodule AsicSentry.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:tesla, "~> 1.11"}
     ]
   end
 
