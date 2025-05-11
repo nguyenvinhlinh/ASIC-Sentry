96018b92160b9de40debf1f0c807170f6acd3c99
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2025-05-11 18:12:43 +0700

    update

--- lib/asic_sentry_web/live/asic_miner_live/index.ex
+++ lib/asic_sentry_web/live/asic_miner_live/index.ex
@@ -7,7 +7,12 @@ defmodule AsicSentryWeb.AsicMinerLive.Index do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, stream(socket, :asic_miners, AsicMiners.list_asic_miners())}
+    asic_miner_list = AsicMiners.list_asic_miners()
+    socket_mod = socket
+    |> assign(:page_title, "ASIC Miner Index")
+    |> stream(:asic_miner_list, asic_miner_list)
+
+    {:ok, socket_mod}
   end
 
   @impl true
@@ -15,27 +20,9 @@ defmodule AsicSentryWeb.AsicMinerLive.Index do
     {:noreply, apply_action(socket, socket.assigns.live_action, params)}
   end
 
-  defp apply_action(socket, :edit, %{"id" => id}) do
-    asic_model_option_list = AsicMiner.get_available_asic_model_list()
-
-    socket
-    |> assign(:page_title, "Edit ASIC Miner")
-    |> assign(:asic_model_option_list, asic_model_option_list)
-    |> assign(:asic_miner, AsicMiners.get_asic_miner!(id))
-  end
-
-  defp apply_action(socket, :new, _params) do
-    asic_model_option_list = AsicMiner.get_available_asic_model_list()
-    socket
-    |> assign(:page_title, "New ASIC Miner")
-    |> assign(:asic_model_option_list, asic_model_option_list)
-    |> assign(:asic_miner, %AsicMiner{})
-  end
-
   defp apply_action(socket, :index, _params) do
     socket
-    |> assign(:page_title, "Listing ASIC Miners")
-    |> assign(:asic_miner, nil)
+
   end
 
   @impl true
@@ -47,7 +34,6 @@ defmodule AsicSentryWeb.AsicMinerLive.Index do
   def handle_event("delete", %{"id" => id}, socket) do
     asic_miner = AsicMiners.get_asic_miner!(id)
     {:ok, _} = AsicMiners.delete_asic_miner(asic_miner)
-
-    {:noreply, stream_delete(socket, :asic_miners, asic_miner)}
+    {:noreply, stream_delete(socket, :asic_miner_list, asic_miner)}
   end
 end
