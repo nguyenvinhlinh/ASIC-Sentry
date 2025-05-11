2bb460a313cd333577b255b0a0c5edcf1579e7c6
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2025-05-11 15:01:59 +0700

    migrate from phoenix 1.7 to 1.8 rc-3

--- lib/asic_sentry_web.ex
+++ lib/asic_sentry_web.ex
@@ -51,8 +51,7 @@ defmodule AsicSentryWeb do
 
   def live_view do
     quote do
-      use Phoenix.LiveView,
-        layout: {AsicSentryWeb.Layouts, :app}
+      use Phoenix.LiveView
 
       unquote(html_helpers())
     end
