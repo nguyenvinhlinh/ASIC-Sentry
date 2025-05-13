c96057018a3be98876447c9f4ea9a85a13ae0909
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2025-05-13 16:48:51 +0700

    realtime update for asic miner index. expected status fetcher also broadcast.

--- lib/asic_sentry_web/live/asic_miner_live/index.ex
+++ lib/asic_sentry_web/live/asic_miner_live/index.ex
@@ -12,22 +12,12 @@ defmodule AsicSentryWeb.AsicMinerLive.Index do
     |> assign(:page_title, "ASIC Miner Index")
     |> stream(:asic_miner_list, asic_miner_list)
 
+    if connected?(socket) do
+      Phoenix.PubSub.subscribe(AsicSentry.PubSub, "asic_miner_index_channel")
+      Phoenix.PubSub.subscribe(AsicSentry.PubSub, "flash_index_channel")
+    end
+
     {:ok, socket_mod}
-  end
-
-  @impl true
-  def handle_params(params, _url, socket) do
-    {:noreply, apply_action(socket, socket.assigns.live_action, params)}
-  end
-
-  defp apply_action(socket, :index, _params) do
-    socket
-
-  end
-
-  @impl true
-  def handle_info({AsicSentryWeb.AsicMinerLive.FormComponent, {:saved, asic_miner}}, socket) do
-    {:noreply, stream_insert(socket, :asic_miners, asic_miner)}
   end
 
   @impl true
@@ -36,10 +26,33 @@ defmodule AsicSentryWeb.AsicMinerLive.Index do
     {:ok, _} = AsicMiners.delete_asic_miner(asic_miner)
 
     message = "ASIC Miner ##{asic_miner.id} deleted."
+    Phoenix.PubSub.broadcast(AsicSentry.PubSub, "asic_miner_index_channel", {:asic_miner_index_channel, :delete, asic_miner})
+    Phoenix.PubSub.broadcast(AsicSentry.PubSub, "flash_index_channel", {:flash_index_channel, :info, message})
+
     socket_mod = socket
     |> put_flash(:info, message)
     |> stream_delete(:asic_miner_list, asic_miner)
 
+    {:noreply, socket_mod}
+  end
+
+  @impl true
+  def handle_info({:asic_miner_index_channel, :create_or_update, asic_miner}, socket) do
+    socket_mod = socket
+    |> stream_insert(:asic_miner_list, asic_miner)
+    {:noreply, socket_mod}
+  end
+
+  @impl true
+  def handle_info({:asic_miner_index_channel, :delete, asic_miner}, socket) do
+    socket_mod = socket
+    |> stream_delete(:asic_miner_list, asic_miner)
+    {:noreply, socket_mod}
+  end
+
+  @impl true
+  def handle_info({:flash_index_channel, flash_type, message}, socket) do
+    socket_mod = put_flash(socket, flash_type, message)
     {:noreply, socket_mod}
   end
 
