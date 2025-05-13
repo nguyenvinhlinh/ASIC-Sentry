449f6f0d727ce9c44b440391819aa94f149a8e3a
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2025-05-13 15:09:11 +0700

    add unit test for new/edit module in asic miner live.

--- test/asic_sentry_web/live/asic_miner_live_test.exs
+++ test/asic_sentry_web/live/asic_miner_live_test.exs
@@ -5,7 +5,7 @@ defmodule AsicSentryWeb.AsicMinerLiveTest do
   import AsicSentry.AsicMinersFixtures
 
   @create_attrs %{ip: "192.168.1.10", api_code: "111-222-333", asic_model: "Ice River - KS5L"}
-  @update_attrs %{ip: "some updated ip", api_code: "222-333-444", asic_model: "Ice River - KS5L"}
+  @update_attrs %{ip: "192.168.1.11", api_code: "222-333-444", asic_model: "Ice River - KS5L", rrc_serial_number: "rrc-serial-no"}
   @invalid_attrs %{ip: nil, api_code: nil, asic_model: "Ice River - KS5L"}
 
   defp create_asic_miner(_) do
@@ -25,7 +25,7 @@ defmodule AsicSentryWeb.AsicMinerLiveTest do
     end
 
     test "redirect to asic miner new page", %{conn: conn} do
-      {:ok, index_live, html} = live(conn, ~p"/asic_miners")
+      {:ok, index_live, _html} = live(conn, ~p"/asic_miners")
       index_live
       |> element("#asic_miner_new")
       |> render_click()
@@ -34,7 +34,7 @@ defmodule AsicSentryWeb.AsicMinerLiveTest do
     end
 
     test "redirect to asic miner edit page", %{conn: conn, asic_miner: asic_miner} do
-      {:ok, index_live, html} = live(conn, ~p"/asic_miners")
+      {:ok, index_live, _html} = live(conn, ~p"/asic_miners")
       index_live
       |> element("#asic_miner-#{asic_miner.id}-edit")
       |> render_click()
@@ -49,56 +49,44 @@ defmodule AsicSentryWeb.AsicMinerLiveTest do
       refute has_element?(index_live, "#asic_miner_list-#{asic_miner.id}")
     end
   end
+
+  test "create new asic miner", %{conn: conn} do
+    {:ok, new_live, new_html} = live(conn, ~p"/asic_miners/new")
+    assert new_html =~ "Create new ASIC Miners"
+
+    assert new_live
+    |> form("#asic_miner_new_form", %{asic_miner: @invalid_attrs})
+    |> render_change() =~ "can&#39;t be blank"
+
+    new_live
+    |> form("#asic_miner_new_form", %{asic_miner: @create_attrs})
+    |> render_submit()
+
+    assert_redirect(new_live, ~p"/asic_miners", 100)
+    {:ok, _index_live, index_html} = live(conn, ~p"/asic_miners")
+    assert index_html =~ "192.168.1.10"
+    assert index_html =~ "111-222-333"
+  end
+
+  test "edit asic miner", %{conn: conn} do
+    asic_miner = asic_miner_fixture()
+
+    {:ok, edit_live, edit_html} = live(conn, ~p"/asic_miners/#{asic_miner.id}/edit")
+    assert edit_html =~ "Edit ASIC Miners"
+
+    assert edit_live
+    |> form("#asic_miner_edit_form", %{asic_miner: @invalid_attrs})
+    |> render_change() =~ "can&#39;t be blank"
+
+    edit_live
+    |> form("#asic_miner_edit_form", %{asic_miner: @update_attrs})
+    |> render_submit()
+
+    assert_redirect(edit_live, ~p"/asic_miners", 100)
+
+    {:ok, _index_live, index_html} = live(conn, ~p"/asic_miners")
+    assert index_html =~ "192.168.1.11"
+    assert index_html =~ "222-333-444"
+    assert index_html =~ "rrc-serial-no"
+  end
 end
-
-
-
-
-    # test "saves new asic_miner", %{conn: conn} do
-    #   {:ok, index_live, _html} = live(conn, ~p"/asic_miners")
-
-    #   assert index_live |> element("a", "New ASIC Miner") |> render_click() =~
-    #     "New ASIC Miner"
-
-    #   assert_patch(index_live, ~p"/asic_miners/new")
-
-    #   assert index_live
-    #   |> form("#asic_miner-form", asic_miner: @invalid_attrs)
-    #   |> render_change() =~ "can&#39;t be blank"
-
-    #   assert index_live
-    #   |> form("#asic_miner-form", asic_miner: @create_attrs)
-    #   |> render_submit()
-
-
-    #   assert_patch(index_live, ~p"/asic_miners")
-
-    #   html = render(index_live)
-    #   assert html =~ "ASIC miner created successfully"
-    #   assert html =~ "111-222-333"
-    #   assert html =~ "192.168.1.10"
-    # end
-
-    # test "updates asic_miner in listing", %{conn: conn, asic_miner: asic_miner} do
-    #   {:ok, index_live, _html} = live(conn, ~p"/asic_miners")
-
-    #   assert index_live |> element("#asic_miners-#{asic_miner.id} a", "Edit") |> render_click() =~
-    #            "Edit ASIC Miner"
-
-    #   assert_patch(index_live, ~p"/asic_miners/#{asic_miner}/edit")
-
-    #   assert index_live
-    #   |> form("#asic_miner-form", asic_miner: @invalid_attrs)
-    #   |> render_change() =~ "can&#39;t be blank"
-
-    #   assert index_live
-    #   |> form("#asic_miner-form", asic_miner: @update_attrs)
-    #   |> render_submit()
-
-    #   assert_patch(index_live, ~p"/asic_miners")
-
-    #   html = render(index_live)
-    #   assert html =~ "ASIC miner updated successfully"
-    #   assert html =~ "some updated ip"
-    #   assert html =~ "222-333-444"
-    # end
