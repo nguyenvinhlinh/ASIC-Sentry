e90ca7037752868fcd0bcde51e1cfa95bb2daf13
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2025-05-13 14:53:41 +0700

    add unit test for asic miner live index mod.

--- test/asic_sentry_web/live/asic_miner_live_test.exs
+++ test/asic_sentry_web/live/asic_miner_live_test.exs
@@ -19,66 +19,86 @@ defmodule AsicSentryWeb.AsicMinerLiveTest do
     test "lists all asic_miners", %{conn: conn, asic_miner: asic_miner} do
       {:ok, _index_live, html} = live(conn, ~p"/asic_miners")
 
-      assert html =~ "ASIC Miner Index"
+      assert html =~ "ASIC Miners"
       assert html =~ asic_miner.ip
+      assert html =~ asic_miner.api_code
     end
 
+    test "redirect to asic miner new page", %{conn: conn} do
+      {:ok, index_live, html} = live(conn, ~p"/asic_miners")
+      index_live
+      |> element("#asic_miner_new")
+      |> render_click()
 
-    test "saves new asic_miner", %{conn: conn} do
-      {:ok, index_live, _html} = live(conn, ~p"/asic_miners")
-
-      assert index_live |> element("a", "New ASIC Miner") |> render_click() =~
-        "New ASIC Miner"
-
-      assert_patch(index_live, ~p"/asic_miners/new")
-
-      assert index_live
-      |> form("#asic_miner-form", asic_miner: @invalid_attrs)
-      |> render_change() =~ "can&#39;t be blank"
-
-      assert index_live
-      |> form("#asic_miner-form", asic_miner: @create_attrs)
-      |> render_submit()
-
-
-      assert_patch(index_live, ~p"/asic_miners")
-
-      html = render(index_live)
-      assert html =~ "ASIC miner created successfully"
-      assert html =~ "111-222-333"
-      assert html =~ "192.168.1.10"
+      assert_redirect(index_live, ~p"/asic_miners/new", 100)
     end
 
-    @tag mustexec: true
-    test "updates asic_miner in listing", %{conn: conn, asic_miner: asic_miner} do
-      {:ok, index_live, _html} = live(conn, ~p"/asic_miners")
+    test "redirect to asic miner edit page", %{conn: conn, asic_miner: asic_miner} do
+      {:ok, index_live, html} = live(conn, ~p"/asic_miners")
+      index_live
+      |> element("#asic_miner-#{asic_miner.id}-edit")
+      |> render_click()
 
-      assert index_live |> element("#asic_miners-#{asic_miner.id} a", "Edit") |> render_click() =~
-               "Edit ASIC Miner"
-
-      assert_patch(index_live, ~p"/asic_miners/#{asic_miner}/edit")
-
-      assert index_live
-      |> form("#asic_miner-form", asic_miner: @invalid_attrs)
-      |> render_change() =~ "can&#39;t be blank"
-
-      assert index_live
-      |> form("#asic_miner-form", asic_miner: @update_attrs)
-      |> render_submit()
-
-      assert_patch(index_live, ~p"/asic_miners")
-
-      html = render(index_live)
-      assert html =~ "ASIC miner updated successfully"
-      assert html =~ "some updated ip"
-      assert html =~ "222-333-444"
+      assert_redirect(index_live, ~p"/asic_miners/#{asic_miner.id}/edit", 100)
     end
 
     test "deletes asic_miner in listing", %{conn: conn, asic_miner: asic_miner} do
       {:ok, index_live, _html} = live(conn, ~p"/asic_miners")
-
-      assert index_live |> element("#asic_miners-#{asic_miner.id} a", "Delete") |> render_click()
-      refute has_element?(index_live, "#asic_miners-#{asic_miner.id}")
+      assert has_element?(index_live, "#asic_miner_list-#{asic_miner.id}")
+      index_live |> element("#asic_miner-#{asic_miner.id}-delete") |> render_click()
+      refute has_element?(index_live, "#asic_miner_list-#{asic_miner.id}")
     end
   end
 end
+
+
+
+
+    # test "saves new asic_miner", %{conn: conn} do
+    #   {:ok, index_live, _html} = live(conn, ~p"/asic_miners")
+
+    #   assert index_live |> element("a", "New ASIC Miner") |> render_click() =~
+    #     "New ASIC Miner"
+
+    #   assert_patch(index_live, ~p"/asic_miners/new")
+
+    #   assert index_live
+    #   |> form("#asic_miner-form", asic_miner: @invalid_attrs)
+    #   |> render_change() =~ "can&#39;t be blank"
+
+    #   assert index_live
+    #   |> form("#asic_miner-form", asic_miner: @create_attrs)
+    #   |> render_submit()
+
+
+    #   assert_patch(index_live, ~p"/asic_miners")
+
+    #   html = render(index_live)
+    #   assert html =~ "ASIC miner created successfully"
+    #   assert html =~ "111-222-333"
+    #   assert html =~ "192.168.1.10"
+    # end
+
+    # test "updates asic_miner in listing", %{conn: conn, asic_miner: asic_miner} do
+    #   {:ok, index_live, _html} = live(conn, ~p"/asic_miners")
+
+    #   assert index_live |> element("#asic_miners-#{asic_miner.id} a", "Edit") |> render_click() =~
+    #            "Edit ASIC Miner"
+
+    #   assert_patch(index_live, ~p"/asic_miners/#{asic_miner}/edit")
+
+    #   assert index_live
+    #   |> form("#asic_miner-form", asic_miner: @invalid_attrs)
+    #   |> render_change() =~ "can&#39;t be blank"
+
+    #   assert index_live
+    #   |> form("#asic_miner-form", asic_miner: @update_attrs)
+    #   |> render_submit()
+
+    #   assert_patch(index_live, ~p"/asic_miners")
+
+    #   html = render(index_live)
+    #   assert html =~ "ASIC miner updated successfully"
+    #   assert html =~ "some updated ip"
+    #   assert html =~ "222-333-444"
+    # end
